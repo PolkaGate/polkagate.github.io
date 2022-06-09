677251dc54e3141743af09f8025d5b5b0b3a3384
Nick-1979 <ekbatanifard@gmail.com>
2022-06-09 14:57:37 +0430

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Avatar, Box, Grid, Typography, Button } from "@mui/material";
+import { useCallback } from "react";
 
 function App() {
+  const handleOpenFirefox = useCallback(() => {
+    window.open(
+      'https://addons.mozilla.org/en-US/firefox/addon/polkadot-js-plus-extension/',
+      "_blank"     
+    );
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container justifyContent="center" sx={{ pt: 5 }}>
+      {/* <Avatar alt="plus" src="images/plus.svg" sx={{ width: 70, height: 70 }} /> */}
+      <Box
+        component="img"
+        alt="logo"
+        src="images/logo.jpg"
+        sx={{
+          width: 405,
+          height: 70,
+          maxHeight: { xs: 35, md: 70 },
+          maxWidth: { xs: 202, md: 405 },
+        }}
+      />
+      <Grid container justifyContent="center">
+        <Box
+          component="img"
+          sx={{
+            height: 607,
+            width: 2245,
+            maxHeight: { xs: 60, md: 303 },
+            maxWidth: { xs: 224, md: 1123 },
+            pt: 3,
+          }}
+          alt="intro"
+          src="images/intro.png"
+        />
+      </Grid>
+      <Grid item xs={12} sx={{ px: 6 }}>
+        <Typography align="center" variant="subtitle1" sx={{ pt: 4 }}>
+          Polkadot js plus is actually Polkadot js extension, plus new features.
+          It is a user-friendly wallet to interact with the Polkadot/Substrate
+          based blockchains through a browser.
+        </Typography>
+        <Typography align="center" variant="body2" sx={{ pt: 2 }}>
+          It allows users to access their account(s), which can also be used to
+          interact with decentralized apps.
+        </Typography>
+      </Grid>
+      <Button
+        color="primary"
+        variant="contained"
+        sx={{ mt: 3 }}
+        startIcon={
+          <Avatar
+            alt="plus"
+            src="images/firefox.png"
+            sx={{ width: 20, height: 20 }}
+          />
+        }
+        onClick={handleOpenFirefox}
+      >
+        Download for firefox
+      </Button>
+    </Grid>
   );
 }
 
