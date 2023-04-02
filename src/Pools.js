23a05f39f1dbaf341eaedacc3f7f78afc82ed248
Kami <ekbatanifard@gmail.com>
2023-04-03 01:30:01 +0330

    Update Pools.js

--- src/Pools.js
+++ src/Pools.js
@@ -21,11 +21,10 @@ function Pools() {
     <Grid container justifyContent="space-between" id='pools' sx={{ color: "white", bgcolor: "#180710", py: '50px' }}>
       <Grid item xs={12} sx={{ textAlign: "center", pb: 2 }}>
         <Typography style={{ display: "inline-block" }} variant="h5" width='100%' fontWeight={700}>
-          Join Our Staking Pools
+          Join our staking pools now!
         </Typography>
         <Typography style={{ display: "inline-block" }} variant="subtitle1" fontWeight={500} width='80%' py='30px'>
-          Polkagate staking pools are available on both Polkadot and Kusama blockchains. You can easily join polls using the extensions, join our Polkadot pool and earn rewards with at least 1 DOT.
-          Similarly you can join the Kusama pool with as little as 0.01 KSM.
+          Polkagate offers staking pools on both the Polkadot and Kusama blockchains, and you can easily become a part of them using the extension. Earn rewards by joining our Polkadot pool with a minimum of 1 DOT, or join our Kusama pool with only 0.01 KSM.
         </Typography>
       </Grid>
       <Pool
